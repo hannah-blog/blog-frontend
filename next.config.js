79bf3366ea05fd896240fe86f9d1d93487536825
Hannah <ghdcoalss33@gmail.com>
2023-02-02 11:38:41 +0900

    feat(git): deploy github page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: "", // production 일때 prefix 경로
+  trailingSlash: true // 빌드 시 폴더 구조 그대로 생성하도록
 }
 
 module.exports = nextConfig
