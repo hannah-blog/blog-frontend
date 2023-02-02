97357a2752890b02ac3fcaff439e1cb41cfe4cce
Hannah <ghdcoalss33@gmail.com>
2023-02-02 18:00:42 +0900

    어영부영 급하게 만들기...

---
+++ components/styles/styleCompnents.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MainWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 300px;
+  width: 100%;
+  color: rgb(26 35 126);
+  margin-top: 7%;
+`;
+
+export const TextWrap = styled.div`
+  width: 43%;
+`;
