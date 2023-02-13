29d546527801ca94f716577a343da6ce43ef226a
Hannah <ghdcoalss33@gmail.com>
2023-02-13 10:16:17 +0900

    feat(seo): site map 설정 파일 이름 수정, description 길이 수정

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 yarn build
 yarn export
 git add out/
-git commit -m "feat(seo): seo, site map 적용해보기"
+git commit -m "feat(seo): site map 설정 파일 이름 수정, description 길이 수정"
 git subtree push --prefix out origin gh-pages
 git checkout gh-pages
 git pull
@@ -13,7 +13,7 @@ cp -rf blog-front/** HongChaeMin.github.io
 cd HongChaeMin.github.io
 git pull
 git add --all
-git commit -m "feat(seo): seo, site map 적용해보기"
+git commit -m "feat(seo): site map 설정 파일 이름 수정, description 길이 수정"
 git push
 cd ../blog-front
 git checkout feat/resume
