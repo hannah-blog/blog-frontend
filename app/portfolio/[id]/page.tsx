06fc79daaac50542f14517941f417aa26db3bac5
Hannah <ghdcoalss33@gmail.com>
2023-12-27 17:57:29 +0900

    feat(portfolio): 썸네일 삭제

--- app/portfolio/[id]/page.tsx
+++ app/portfolio/[id]/page.tsx
@@ -1,5 +1,4 @@
 import styles from '@/styles/app/blog/id/page.module.css'
-import Image from 'next/image'
 import HeadMeta from '@/components/utils/meta-head'
 import Markdown from '@/components/utils/markdown'
 import ProgressBar from '@/components/motion/progress-bar'
@@ -24,13 +23,6 @@ export default async function PortfolioDetail({
 			<div className={styles.titleText}>{portfolio.title}</div>
 			<div className={styles.dateWrapper}>
 			</div>
-			<Image
-				className={styles.thumbnail}
-				src={portfolio.thumbnailUrl}
-				alt={`${portfolio.id}-thumbnail-image`}
-				width={500}
-				height={500}
-			/>
 			<div className={styles.postWrapper}>
 				<Markdown content={portfolio.content}/>
 			</div>
