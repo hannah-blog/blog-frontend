6e955d794802293e9a1efd01b4a7434b96b8fd8c
Hannah <ghdcoalss33@gmail.com>
2024-01-22 19:07:01 +0900

    fix: 목차 없는거 안보이게 수정

--- components/post/side/idx-box.tsx
+++ components/post/side/idx-box.tsx
@@ -12,14 +12,16 @@ export default function IdxBox({ content, id }: { content: string, id: number })
 	const result = getIdxList(content);
 
 	return <div className={styles.idxWrapper}>
-		<Typography variant="h6" color="gray">목차</Typography>
-		{result.map((item, idx) => {
-			return <Typography key={idx} color="gray" className={`${styles[`idxTap${item.idx}`]} ${styles.idx}`}>
-				<Link href={`${process.env.NEXT_PUBLIC_WWW_URL}/develop/blog/${id}/${item.link}`}>
-					{item.line}
-				</Link>
-			</Typography>;
-		})}
+		{ result.length != 0 && <>
+      <Typography variant="h6" color="gray">목차</Typography>
+			{ result.map((item, idx) => {
+				return <Typography key={idx} color="gray" className={`${styles[`idxTap${item.idx}`]} ${styles.idx}`}>
+					<Link href={`${process.env.NEXT_PUBLIC_WWW_URL}/develop/blog/${id}/${item.link}`}>
+						{item.line}
+					</Link>
+				</Typography>;
+			})}
+    </> }
 	</div>;
 }
 
