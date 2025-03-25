ee3ad67b508e9da469a1d875e79c3a089c3be521
홍채민 <coals0329@reindeers.com>
2025-03-25 14:56:24 +0900

    feat: upgrade version

--- components/motion/helper/circle-animation.tsx
+++ components/motion/helper/circle-animation.tsx
@@ -3,44 +3,56 @@ import { Keyframes } from '@/components/motion/keyframes'
 import { Tooltip } from '@/components/tailwind/client-components'
 
 export default function CircleAnimation() {
-	return  <div className={styles.animationWrapper}>
-		{ Array.from({length: 10}).map((_, i) => {
-			const size = getRandomInt(20, 200);
-			return <>
-				<Keyframes
-					name={`svg-animation-${i}`}
-					delay={`${getRandomInt(100, 200) / 100}s`}
-					_0={{ marginBottom: '2px' }}
-					_70={{ opacity: 0.2 }}
-					_100={{ opacity: 0.3, marginBottom: `${getRandomInt(50, 500)}px`}}
-				/>
-				<Tooltip
-					content="🥰"
-					animate={{
-						mount: { scale: 1, y: 0 },
-						unmount: { scale: 0, y: 25 },
-					}}
-				>
-					<Circle key={i} className={`svg-animation-${i}`} width={size} height={size} fill={getRandomColor()} />
-				</Tooltip>
-			</>;
-		})}
-	</div>
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return <div className={styles.animationWrapper}>
+    {array.map((_, i) => {
+      const size = getRandomInt(20, 200);
+      return <div key={i}>
+        <Keyframes
+          name={`svg-animation-${i}`}
+          delay={`${getRandomInt(100, 200) / 100}s`}
+          _0={{marginBottom: '2px'}}
+          _70={{opacity: 0.2}}
+          _100={{opacity: 0.3, marginBottom: `${getRandomInt(50, 500)}px`}}
+        />
+        <Tooltip
+          content="🥰"
+          animate={{
+            mount: {scale: 1, y: 0},
+            unmount: {scale: 0, y: 25},
+          }}
+        >
+          <Circle
+            key={i}
+            className={`svg-animation-${i}`}
+            width={size}
+            height={size}
+            fill={getRandomColor()}
+          />
+        </Tooltip>
+      </div>;
+    })}
+  </div>
 }
 
-const Circle = ({ width, height, fill, className }: { width: number, height: number, fill: string, className: string }) => {
-	return <svg width={width} height={height} className={className}>
-		<circle cx={width / 2} cy={width / 2} r={width / 2} fill={fill} />
-	</svg>
+const Circle = ({width, height, fill, className}: {
+  width: number,
+  height: number,
+  fill: string,
+  className: string
+}) => {
+  return <svg width={width} height={height} className={className}>
+    <circle cx={width / 2} cy={width / 2} r={width / 2} fill={fill}/>
+  </svg>
 }
 
 const getRandomInt = (min: number, max: number) => {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const getRandomColor = () => {
-	const hexArray = ['#ccc4e1', '#d3d4e4', '#bed7db', '#a0c7d8',
-		'#e4e8e7', '#e8ddea', '#efd6d2',
-		'#f3dfe2', '#f3f1ce'];
-	return hexArray[Math.floor(Math.random() * hexArray.length)];
+  const hexArray = ['#ccc4e1', '#d3d4e4', '#bed7db', '#a0c7d8',
+    '#e4e8e7', '#e8ddea', '#efd6d2',
+    '#f3dfe2', '#f3f1ce'];
+  return hexArray[Math.floor(Math.random() * hexArray.length)];
 }
